9641a9ea7d0ed04362253a8d5dfde44fabfe2a45
髙橋和晃 <kazuaki.flute@gmail.com>
2020-11-01 16:02:17 +0900

    add capsule controller script

--- Assets/Scripts/CapsuleController.cs
+++ Assets/Scripts/CapsuleController.cs
@@ -7,7 +7,7 @@ public class CapsuleController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
