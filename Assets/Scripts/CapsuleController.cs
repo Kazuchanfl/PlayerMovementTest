ea5dcd4fb156cbfb4ae91d397f6a18252457b2e8
髙橋和晃 <kazuaki.flute@gmail.com>
2020-11-03 16:21:49 +0900

    change key direcrtion

--- Assets/Scripts/CapsuleController.cs
+++ Assets/Scripts/CapsuleController.cs
@@ -20,18 +20,18 @@ public class CapsuleController : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
         CapsuleCollider cc = GetComponent<CapsuleCollider>();
 
-        // Move right when Right Arrow is held down
-        if (Input.GetKey(KeyCode.RightArrow))
+        // Move right when Up Arrow is held down
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(Vector3.right * 10f);
-            print("right arrow key is held down");
+            print("up arrow key is held down");
         }
 
-        // Move left when Left Arrow is held down
-        if (Input.GetKey(KeyCode.LeftArrow))
+        // Move left when Down Arrow is held down
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             rb.AddForce(Vector3.left * 10f);
-            print("left arrow key is held down");
+            print("down arrow key is held down");
         }
 
         // Jump when Left Shift is held down
